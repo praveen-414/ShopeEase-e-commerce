b3329c0402d2511029bec18fa7be7c1024810e0f
praveen-414 <praveenkumar.dev21@gmail.com>
2026-07-22 12:51:38 +0530

    update frontend url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://shopeease-e-commerce.onrender.com",
     credentials: true,
   }),
 );
