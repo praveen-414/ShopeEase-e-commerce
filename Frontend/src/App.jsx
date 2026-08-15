d4e0ed8d403fed6830b372789293e3e3cf085d07
praveen-414 <praveenkumar.dev21@gmail.com>
2026-08-15 18:48:35 +0530

    fix errors

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Toaster } from "react-hot-toast";
 import AppLayout from "./Layout/AppLayout";
 import Categories from "./Pages/Categories";
@@ -21,20 +21,21 @@ import Checkout from "./Pages/Checkout";
 import Payment from "./Pages/Payment";
 import OrderSuccess from "./Pages/OrderSuccess";
 import MyOrders from "./Pages/MyOrders";
-import { useDispatch } from "react-redux";
 import api from "./config/axios";
 import { setCart } from "./redux/slices/cartSlice";
 import { setProducts } from "./redux/slices/productSlice";
-import { Navigate } from "react-router-dom";
 import { setUser } from "./redux/slices/userSlice";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   const { theme } = useSelector((state) => state.theme);
   const { user } = useSelector((state) => state.user);
+
   const [loading, setLoading] = useState(true);
+
   const dispatch = useDispatch();
 
+  // Dark mode
   useEffect(() => {
     if (theme === "dark") {
       document.documentElement.classList.add("dark");
@@ -43,20 +44,18 @@ const App = () => {
     }
   }, [theme]);
 
+  // Load user and products only once
   useEffect(() => {
-  const loadData = async () => {
-    try {
-      await Promise.all([
-        getCurrentUser(),
-        getProducts(),
-      ]);
-    } finally {
-      setLoading(false);
-    }
-  };
+    const loadData = async () => {
+      try {
+        await Promise.all([getCurrentUser(), getProducts()]);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  loadData();
-}, []);
+    loadData();
+  }, []);
 
   const getCurrentUser = async () => {
     try {
@@ -67,7 +66,7 @@ const App = () => {
       dispatch(setUser(res.data));
     } catch (error) {
       dispatch(setUser(null));
-    } 
+    }
   };
 
   const getCartProducts = async () => {
@@ -78,8 +77,9 @@ const App = () => {
 
       dispatch(setCart(res.data.cart.items));
     } catch (error) {
-      console.log(error);
-    } 
+      console.log("Cart error:", error.response?.data || error.message);
+      dispatch(setCart([]));
+    }
   };
 
   const getProducts = async () => {
@@ -88,21 +88,18 @@ const App = () => {
 
       dispatch(setProducts(res.data.products));
     } catch (error) {
-      console.log(error);
-    } 
+      console.log("Products error:", error.response?.data || error.message);
+    }
   };
-  useEffect(() => {
-    getCurrentUser();
-    getProducts();
-  }, []);
 
+  // Get cart whenever user changes
   useEffect(() => {
     if (user) {
       getCartProducts();
+    } else {
+      dispatch(setCart([]));
     }
   }, [user]);
-
-  
 
   if (loading) {
     return (
@@ -111,6 +108,7 @@ const App = () => {
       </div>
     );
   }
+
   const routes = createBrowserRouter([
     {
       path: "/",
@@ -211,10 +209,11 @@ const App = () => {
       element: <Login />,
     },
   ]);
+
   return (
     <>
       <Toaster position="top-center" reverseOrder={false} />
-      <RouterProvider router={routes}></RouterProvider>
+      <RouterProvider router={routes} />
     </>
   );
 };
